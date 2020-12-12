0529f8953ca7430ba7316dcc07b83de89270f27e
takekawa128 <shion2002@live.jp>
2020-12-12 09:52:30 +0000

    12/12

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
 module Pictgram
-　class Application < Rails::Application
-　config.load_defaults 5.2
-　# 下に追加
-　　 config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-　end
+  class Application < Rails::Application
+  config.load_defaults 5.2
+  # 下に追加
+  config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+  end
 end
