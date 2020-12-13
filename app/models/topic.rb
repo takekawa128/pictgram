2783ca841076a157188bf2b49b86fa3e1b0b5075
takekawa128 <shion2002@live.jp>
2020-12-13 09:20:02 +0000

    いいね済みかどうかによって表示を変える

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,4 +6,8 @@ class Topic < ApplicationRecord
   belongs_to :user
   
   mount_uploader :image, ImageUploader
+  
+  #いいねした場合にハートボタンの表示を消すようにコードを変更
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
 end
