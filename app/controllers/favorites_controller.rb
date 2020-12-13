f99081f20673ed5b349938556204d5ce1d2d2372
takekawa128 <shion2002@live.jp>
2020-12-13 08:37:38 +0000

    コントローラ、ルーターの作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,15 @@ class FavoritesController < ApplicationController
     @favorite_topics = current_user.favorite_topics
   end
 
+#Favoritesクラスのインスタンスfavoriteを生成する
   def create
     favorite = Favorite.new
+#favorite.user_idに現在ログインしているuser(current_user)のidを保存する
     favorite.user_id = current_user.id
+#favorite.topic_idにparams[:topic_id]（後述）を保存する
     favorite.topic_id = params[:topic_id]
 
+#favoriteをsaveし、その成否によって条件分岐する
     if favorite.save
       redirect_to topics_path, success: 'お気に入りに登録しました'
     else
